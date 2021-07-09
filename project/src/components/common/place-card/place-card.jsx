ff1c95c7f574a0fd403e99f67f96b5e28f8ae032
dmitriy.lobasov <dmitriy.lobasov@shtormtech.ru>
2021-07-09 16:59:33 +0300

    добавляет обработчки на кнопку добавления в избранное

--- project/src/components/common/place-card/place-card.jsx
+++ project/src/components/common/place-card/place-card.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom';
 import { hoverCityCard } from '../../../store/action';
 import { connect } from 'react-redux';
 
-function PlaceCard({ price, previewImage, title, apartmentType, rating, onCardHover, id, index }) {
+function PlaceCard({ price, previewImage, title, apartmentType, rating, onCardHover, id, index, isFavorite }) {
   const getOfferPath = (roomId) => `/offer/${roomId}`;
+
+  const handleFavoriteClick = () => {
+    console.log(id);
+  };
 
   return (
     <article
@@ -24,7 +28,11 @@ function PlaceCard({ price, previewImage, title, apartmentType, rating, onCardHo
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+          <button
+            onClick={handleFavoriteClick}
+            className={`place-card__bookmark-button button ${isFavorite ? 'place-card__bookmark-button--active' : ''}`}
+            type="button"
+          >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -58,6 +66,7 @@ PlaceCard.propTypes = {
     PropTypes.number,
   ]),
   index: PropTypes.number,
+  isFavorite: PropTypes.bool,
 };
 
 const mapDispatchToProps = (dispatch) => ({
--- project/src/components/pages/main/main.jsx
+++ project/src/components/pages/main/main.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import OffersList from "../../common/offers-list/offers-list";
-import Map from "../../common/map/map";
-import getCurrentCityOffers from "../../../getCurrentCityOffers";
-import { CityType, sortOptions } from "../../../const";
-import { offerProp } from "../../app/app.prop";
-import { connect } from "react-redux";
-import { changeCity } from "../../../store/action";
-import CitiesList from "./cities-list";
-import Header from "../../common/header/header";
-import MainEmpty from "./main-empty";
-import SortOptions from "./sort-options";
-import { getCurrentCity } from "../../../store/offers/selectors";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import OffersList from '../../common/offers-list/offers-list';
+import Map from '../../common/map/map';
+import getCurrentCityOffers from '../../../getCurrentCityOffers';
+import { CityType, sortOptions } from '../../../const';
+import { offerProp } from '../../app/app.prop';
+import { connect } from 'react-redux';
+import { changeCity } from '../../../store/action';
+import CitiesList from './cities-list';
+import Header from '../../common/header/header';
+import MainEmpty from './main-empty';
+import SortOptions from './sort-options';
+import { getCurrentCity } from '../../../store/offers/selectors';
 
 function Main({ offers, city = CityType.PARIS, onCityChange }) {
   const { TOP_RATED_FIRST, PRICE_TO_HIGH, PRICE_TO_LOW, POPULAR } = sortOptions;
@@ -24,9 +24,7 @@ function Main({ offers, city = CityType.PARIS, onCityChange }) {
   const sortOffers = (sortOption) => {
     const currentCityOffers = currentCityData ? currentCityData.offers : [];
 
-    const sortedOffersByPrice = currentCityOffers.sort(
-      (firstOffer, secondOffer) => secondOffer.price - firstOffer.price
-    );
+    const sortedOffersByPrice = currentCityOffers.sort((firstOffer, secondOffer) => secondOffer.price - firstOffer.price);
 
     switch (sortOption) {
       case PRICE_TO_LOW:
@@ -34,9 +32,7 @@ function Main({ offers, city = CityType.PARIS, onCityChange }) {
       case PRICE_TO_HIGH:
         return sortedOffersByPrice.reverse();
       case TOP_RATED_FIRST:
-        return currentCityOffers.sort(
-          (firstOffer, secondOffer) => secondOffer.rating - firstOffer.rating
-        );
+        return currentCityOffers.sort((firstOffer, secondOffer) => secondOffer.rating - firstOffer.rating);
       default:
         return currentCityOffers;
     }
@@ -47,17 +43,17 @@ function Main({ offers, city = CityType.PARIS, onCityChange }) {
   const handleSortOptionClick = (option) => setCurrentSortOption(option);
 
   return (
-    <div className="page page--gray page--main">
+    <div className='page page--gray page--main'>
       <Header />
-      <main className="page__main page__main--index">
-        <h1 className="visually-hidden">Cities</h1>
+      <main className='page__main page__main--index'>
+        <h1 className='visually-hidden'>Cities</h1>
         <CitiesList currentCity={city} onCityChange={onCityChange} />
-        <div className="cities">
+        <div className='cities'>
           {sortedOffers.length ? (
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">
+            <div className='cities__places-container container'>
+              <section className='cities__places places'>
+                <h2 className='visually-hidden'>Places</h2>
+                <b className='places__found'>
                   {sortedOffers.length} places to stay in {city}
                 </b>
                 <SortOptions
@@ -66,8 +62,8 @@ function Main({ offers, city = CityType.PARIS, onCityChange }) {
                 />
                 <OffersList offers={sortedOffers} />
               </section>
-              <div className="cities__right-section">
-                <section className="cities__map map">
+              <div className='cities__right-section'>
+                <section className='cities__map map'>
                   {currentCityData && (
                     <Map
                       offers={sortedOffers}
